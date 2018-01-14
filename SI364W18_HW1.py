5a90678310f1d88f8fd7bccea9e7cf6284333928
yuxuanc <yuxuanc@umich.edu>
2018-01-14 15:56:30 -0500

    finish hw

--- SI364W18_HW1.py
+++ SI364W18_HW1.py
@@ -6,18 +6,97 @@
 
 ## List below here, in a comment/comments, the people you worked with on this assignment AND any resources you used to find code (50 point deduction for not doing so). If none, write "None".
 
+# worked with Rachel Chang
+# first_flask_app_solution.py from lecture 1
+# challenges.py from first discussion
+# example.py and samepage.py from lecture 2
 
 
 ## [PROBLEM 1] - 150 points
 ## Below is code for one of the simplest possible Flask applications. Edit the code so that once you run this application locally and go to the URL 'http://localhost:5000/class', you see a page that says "Welcome to SI 364!"
 
-from flask import Flask
+from flask import Flask, request
+import requests
+import json
+
 app = Flask(__name__)
 app.debug = True
+
 
 @app.route('/')
 def hello_to_you():
     return 'Hello!'
+
+
+# Problem 1
+@app.route('/class')
+def welcome():
+    return 'Welcome to SI 364!'
+
+# Problem 2
+@app.route('/movie/<movie_name>')
+def movie_data(movie_name):
+    baseurl = "https://itunes.apple.com/search"
+    return requests.get(baseurl, params = {"term": movie_name, "media": "movie"}).text
+
+# Problem 3
+@app.route('/question')
+def question():
+    form = """<!DOCTYPE html>
+<html>
+<body>
+<form action="/doubledNumber" method="post">
+  Enter a number:<br>
+  <input type="number" name="number">
+  <br>
+  <input type="submit" value="Submit">
+</form>
+</body>
+</html>"""
+    return form
+
+@app.route('/doubledNumber', methods = ['POST'])
+def displayData():
+    if request.method == 'POST':
+        number = int(request.form.get("number"))
+        return "Double your favorite number is " + str(number * 2)
+
+# # Problem 4
+@app.route('/problem4form', methods = ['GET', 'POST'])
+def pokemon_explorer():
+    form = """<!DOCTYPE html>
+<html>
+<head>
+    <title>Pokemon info finder</title>
+</head>
+<body>
+<form method="post">
+  Enter Pokemon id or name:
+  <input type="text" name="name"><br>
+  Display Pokemon's:<br>
+  <input type="checkbox" name="id" value="id">id<br>
+  <input type="checkbox" name="height" value="height">height<br>
+  <input type="checkbox" name="weight" value="weight">weight<br>
+  <input type="checkbox" name="base_experience" value="base_experience">base experience<br>
+  <input type="submit" value="Submit">
+</form>
+</body>
+</html>"""
+    if request.method == "POST":
+        # get data from pokemon API, which is supposed to return a json object
+        pokemonData = requests.get('http://pokeapi.co/api/v2/pokemon/' + request.form["name"]).text
+        pokemonJson = json.loads(pokemonData)
+
+        result = ''
+        for option in request.form:
+            # get relevant information from json
+            result += option + ': ' + str(pokemonJson[option]) + '<br>'
+
+        # display result and show form again
+        return result + '<br>' + form
+    else:
+        return form
+
 
 
 if __name__ == '__main__':
